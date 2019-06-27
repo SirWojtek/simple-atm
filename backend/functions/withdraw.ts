080b6ab2b3b517eb19ece4a86164f65f1a436e16
Mateusz Okulewicz <mat@citrusad.com>
2019-06-27 21:31:42 +0200

    withdraw algorithm

--- backend/functions/withdraw.ts
+++ backend/functions/withdraw.ts
@@ -4,16 +4,37 @@ export interface WithdrawInput {
 
 export interface WithdrawResponse {
   amount: number;
-  bills: number[];
+  notes: number[];
 }
 
+const NOTES = [100, 50, 20, 10];
+
 export function withdraw(input: WithdrawInput): WithdrawResponse {
+  if (typeof input.amount !== 'number') {
+    throw Error('Amount is not a number');
+  }
   if (!input.amount) {
     return {
       amount: input.amount,
-      bills: [],
+      notes: [],
     };
   }
 
-  return null;
+  const notes = [];
+  let workingAmount = input.amount;
+  let note;
+
+  while ((note = NOTES.find(n => n <= workingAmount))) {
+    notes.push(note);
+    workingAmount -= note;
+  }
+
+  if (workingAmount) {
+    throw Error('Cannot withdraw this amount');
+  }
+
+  return {
+    amount: input.amount,
+    notes,
+  };
 }

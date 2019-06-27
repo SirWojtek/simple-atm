afd8dbc90ffe43e8507c79df6a1d910f3de1a3f4
Mateusz Okulewicz <mat@citrusad.com>
2019-06-27 20:36:36 +0200

    basic view

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AtmComponent } from './atm/atm.component';
 
 @NgModule({
   declarations: [AppComponent, AtmComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatCardModule],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- frontend/src/app/atm/atm.component.ts
+++ frontend/src/app/atm/atm.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-atm',
-  templateUrl: './atm.component.html'
+  templateUrl: './atm.component.html',
+  styleUrls: ['./atm.component.scss']
 })
-export class AtmComponent {}
+export class AtmComponent {
+  cashInputControl = this.formBuilder.control(null, Validators.required);
+
+  responses = [];
+
+  constructor(private formBuilder: FormBuilder) {}
+}

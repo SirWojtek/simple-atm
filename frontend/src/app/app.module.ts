87b74dbe2060b19ee8b4a84e38f3380fe86aebfc
Mateusz Okulewicz <mat@citrusad.com>
2019-06-27 16:05:01 +0200

    a simple frame

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AtmComponent } from './atm/atm.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [AppComponent, AtmComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatCardModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ frontend/src/app/atm/atm.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-atm',
+  templateUrl: './atm.component.html'
+})
+export class AtmComponent {}

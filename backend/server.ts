3b32df6555509411138dbcb61dfc8d3196a6bea5
Mateusz Okulewicz <mat@citrusad.com>
2019-06-28 08:44:08 +0200

    cors

--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
 
 import {
   WithdrawInput,
@@ -11,7 +12,26 @@ import {
 
 const app = express();
 
+const allowedOrigins = ['http://localhost:4200', 'http://simple-atm.com'];
+
 app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // NOTE: allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          'The CORS policy does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+
+      return callback(null, true);
+    },
+  }),
+);
 
 app.post('/withdraw', (req, res) => {
   const input: WithdrawInput = req.body;
